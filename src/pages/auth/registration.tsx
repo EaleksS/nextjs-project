69823fce7a8462f898518475b0317c6b7566a39f
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-03-12 23:59:47 +0900

    .

--- src/pages/auth/registration.tsx
+++ src/pages/auth/registration.tsx
@@ -6,18 +6,13 @@ import CustomCheckbox from '@/Components/UiKit/CheckBox/CheckBox';
 import { routerConstants } from '@/Constants/RouterConstants';
 import Link from 'next/link';
 import Layout from '../Layout';
-<<<<<<< Updated upstream
-=======
 import { useAuthStore } from '@/store/store';
 
 import { useForm } from 'react-hook-form';
->>>>>>> Stashed changes
 
 const Registration = () => {
   const [step, setStep] = useState<1 | 2>(1);
 
-<<<<<<< Updated upstream
-=======
   const { getRegister } = useAuthStore();
 
   const [isPassword, setIsPassword] = useState('');
@@ -39,7 +34,6 @@ const Registration = () => {
   const onSubmit = (data: any) =>
     getRegister(data.email, data.phone, data.username, data.password);
 
->>>>>>> Stashed changes
   const registrationStep = () => {
     const stepHandler = (step: 1 | 2) => {
       setStep(step);
@@ -64,41 +58,9 @@ const Registration = () => {
             className={`auth-container ${styles['registration-container']} ${styles['registration-container__step2-container']}`}
           >
             <h1>Регистрация</h1>
-<<<<<<< Updated upstream
-
-            <div className={styles['input-container']}>
-              {/* <Input placeholder={'Имя'} />
-              <Input placeholder={'Номер телефона'} />
-              <Input placeholder={'Почта'} />
-              <Input placeholder={'Введите пароль'} />
-              <Input placeholder={'Повторите пароль'} /> */}
-              <input className={styles.input} type="text" placeholder="Имя" />
-              <input
-                className={styles.input}
-                type="text"
-                placeholder="Номер телефона"
-              />
-              <input className={styles.input} type="mail" placeholder="Почта" />
-              <input
-                className={styles.input}
-                type="password"
-                placeholder="Введите пароль"
-              />
-              <input
-                className={styles.input}
-                type="password"
-                placeholder="Повторите пароль"
-              />
-            </div>
-            <div
-              className={
-                styles['registration-container__step2-container__footer-block']
-              }
-=======
             <form
               className={styles['input-container']}
               onSubmit={handleSubmit(onSubmit)}
->>>>>>> Stashed changes
             >
               {/* USERNAME */}
               <label>
@@ -222,12 +184,7 @@ const Registration = () => {
                   Продолжить
                 </Button>
               </div>
-<<<<<<< Updated upstream
-              <Button href={routerConstants.CONFIRM_MOBILE}>Продолжить</Button>
-            </div>
-=======
             </form>
->>>>>>> Stashed changes
           </div>
         );
       }
