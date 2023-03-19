8b03c7add0eee384eeb78b6d8d7f28ceb8ce3158
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-03-19 18:25:58 +0900

    удалил лишние зависимости

--- src/Components/EntriesPage/EntriesModal/DateSelect/DateSelect.tsx
+++ src/Components/EntriesPage/EntriesModal/DateSelect/DateSelect.tsx
@@ -1,8 +1,4 @@
 import React, { useState } from 'react';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { TimePicker, MobileTimePicker } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import styles from './DateSelect.module.scss';
 import { DatePicker } from 'rsuite';
 
@@ -14,33 +10,7 @@ const DateSelect = () => {
 
   return (
     <>
-      {/* <LocalizationProvider dateAdapter={AdapterDayjs}> */}
       <div className={styles.container}>
-        {/* <DatePicker
-            className={`${styles.DatePicker} ${
-              selectedDate !== null && styles.Active
-            }`}
-            label="Дата:"
-            value={selectedDate}
-            onChange={(newValue) => setSelectedDate(newValue)}
-          /> */}
-        {/* <TimePicker
-            className={`${styles.TimePicker} ${
-              selectedTimeBegin !== null && styles.Active
-            }`}
-            label="Начало:"
-            value={selectedTimeBegin}
-            onChange={(newValue) => setSelectedTimeBegin(newValue)}
-          />
-          <MobileTimePicker
-            className={`${styles.MobileTimePicker} ${
-              selectedTimeEnd !== null && styles.Active
-            }`}
-            label="Конец:"
-            value={selectedTimeEnd}
-            onChange={(newValue) => setSelectedTimeEnd(newValue)}
-          /> */}
-
         <DatePicker
           style={{ width: 300 }}
           placeholder="Дата:"
@@ -88,7 +58,6 @@ const DateSelect = () => {
           editable={false}
         />
       </div>
-      {/* </LocalizationProvider> */}
     </>
   );
 };
