edcb06e041b6484dada5c6663f1d75b400fdc5c6
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-03-19 14:37:00 +0900

    Теперь можно выбрать только вперед на 3 месяца

--- src/Components/EntriesPage/EntriesModal/DateSelect/DateSelect.tsx
+++ src/Components/EntriesPage/EntriesModal/DateSelect/DateSelect.tsx
@@ -10,6 +10,7 @@ const DateSelect = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [selectedTimeBegin, setSelectedTimeBegin] = useState<Date | null>(null);
   const [selectedTimeEnd, setSelectedTimeEnd] = useState<Date | null>(null);
+  const [errormsg, setErrormsg] = useState('');
 
   return (
     <>
@@ -44,9 +45,30 @@ const DateSelect = () => {
           style={{ width: 300 }}
           placeholder="Дата:"
           value={selectedDate}
-          onChange={(e: any) => setSelectedDate(e)}
+          onChange={(e: any) => {
+            // e > new Date().setMonth(new Date().getMonth() + 3)
+            //   ? setErrormsg('Дату можно выбрать только на 3 месяца вперед')
+            //   : e > new Date().setDate(new Date().getDate() - 1)
+            //   ? setSelectedDate(e) setErrormsg('')
+            //   : setErrormsg('Нельзя выбрать дату только вперед');
+            if (e > new Date().setMonth(new Date().getMonth() + 3)) {
+              setSelectedDate(null);
+              setErrormsg('Дату можно выбрать только на 3 месяца вперед');
+            } else {
+              if (e > new Date().setDate(new Date().getDate() - 1)) {
+                setSelectedDate(e);
+                setErrormsg('');
+              } else {
+                setSelectedDate(null);
+                setErrormsg('Нельзя выбрать дату только вперед');
+              }
+            }
+          }}
           editable={false}
         />
+
+        <p className={styles.errormsg}>{errormsg}</p>
+
         <DatePicker
           format="HH:mm"
           ranges={[]}
