95a62d541991e1531a259c5dbfdd60ae8d4edba3
Роман Барабанов <wrohw@MacBook-Air-Roman.local>
2023-03-20 11:21:54 +0300

    Экстренные вызовы

--- src/pages/call/index.tsx
+++ src/pages/call/index.tsx
@@ -4,12 +4,11 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import car from "../../Assets/images/car.png";
 import hospitalImg from "../../Assets/images/hospital.png";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 type Props = {};
 
 const hospital = (props: Props) => {
-  const router = useRouter();
   return (
     <motion.div className={styles.container}>
       <motion.div
@@ -22,35 +21,28 @@ const hospital = (props: Props) => {
       >
         <p>Экстренные контакты</p>
         <div className={styles.container_for_image}>
-          <div
-            className={styles.image_cont}
-            onClick={() => {
-              router.push("/call/1");
-            }}
-          >
-            <Image src={car} alt="" className={styles.car_img} />
-          </div>
-          <div
-            className={styles.image_cont}
-            onClick={() => {
-              router.push("/call/2");
-            }}
-          >
-            <Image src={hospitalImg} alt="" className={styles.hospital_img} />
-          </div>
+          <Link href="/call/1">
+            <div className={styles.image_cont}>
+              <Image src={car} alt="" className={styles.car_img} />
+            </div>
+          </Link>
+          <Link href="/call/2">
+            <div className={styles.image_cont}>
+              <Image src={hospitalImg} alt="" className={styles.hospital_img} />
+            </div>
+          </Link>
         </div>
       </motion.div>
-      <motion.div
-        className={styles.go_out_cont}
-        onClick={() => {
-          router.push("/");
-        }}
-        animate={{
-          transition: { duration: 0.6, delay: 0.2 },
-          opacity: 1,
-          y: -5,
-        }}
-      />
+      <Link href="/">
+        <motion.div
+          className={styles.go_out_cont}
+          animate={{
+            transition: { duration: 0.6, delay: 0.2 },
+            opacity: 1,
+            y: -5,
+          }}
+        />
+      </Link>
     </motion.div>
   );
 };
