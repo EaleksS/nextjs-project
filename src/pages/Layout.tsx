08a583f290a98f6d5a75715b804e5f9d9eb9be65
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-03-21 20:09:00 +0900

    .

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -41,10 +41,10 @@ const Layout: FC<ILayout> = ({
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <meta
+        <meta
           http-equiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
-        ></meta> */}
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={roboto.className}>
