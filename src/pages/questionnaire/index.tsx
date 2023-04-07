b837f4e54601a11a43028de1c2a268711836051e
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-04-07 18:05:58 +0900

    .

--- src/pages/questionnaire/index.tsx
+++ src/pages/questionnaire/index.tsx
@@ -82,66 +82,66 @@ const Questionnaire = () => {
                 <h1>Название</h1>
               </div>
             </div>
-            <div className={styles.items}>
-              <div className={styles.title}>
-                <h1>Февраль</h1>
-                <h1>2023</h1>
+          </div>{' '}
+          <div className={styles.items}>
+            <div className={styles.title}>
+              <h1>Февраль</h1>
+              <h1>2023</h1>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Вт <br /> 1
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Вт <br /> 1
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Ср <br /> 7
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Ср <br /> 7
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Чт <br /> 9
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Пт <br /> 11
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Чт <br /> 9
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Сб <br /> 15
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Вс <br /> 17
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Пт <br /> 11
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Пн <br /> 21
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Сб <br /> 15
+              </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Вс <br /> 17
+              </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Пн <br /> 21
+              </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
             </div>
           </div>
@@ -217,66 +217,66 @@ const Questionnaire = () => {
                 <h1>Название</h1>
               </div>
             </div>
-            <div className={styles.items}>
-              <div className={styles.title}>
-                <h1>Февраль</h1>
-                <h1>2023</h1>
+          </div>{' '}
+          <div className={styles.items}>
+            <div className={styles.title}>
+              <h1>Февраль</h1>
+              <h1>2023</h1>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Вт <br /> 1
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Вт <br /> 1
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Ср <br /> 7
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Ср <br /> 7
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Чт <br /> 9
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Пт <br /> 11
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Чт <br /> 9
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Сб <br /> 15
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Вс <br /> 17
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Пт <br /> 11
               </div>
-              <div className={styles.item}>
-                <div className={styles.date}>
-                  Пн <br /> 21
-                </div>
-                <div className={styles.name}>
-                  <h1>Название</h1>
-                </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Сб <br /> 15
+              </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Вс <br /> 17
+              </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.date}>
+                Пн <br /> 21
+              </div>
+              <div className={styles.name}>
+                <h1>Название</h1>
               </div>
             </div>
           </div>
